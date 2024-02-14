f9e580791907e96f4aa1805cac2a8b7e2c52c280
Maksim <sharkomaks@gmail.com>
2024-02-14 23:05:04 +0300

    Домашнее задание - Циклы

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,4 +1,4 @@
-const arr = [1, 40, -5, 10, 0];
+const arr = [1, 40, -5, 10, 0, -11, 90, 15];
 
 function sort(array) {
     const arraySort = [];
@@ -6,7 +6,7 @@ function sort(array) {
         let current = array[i];
         let index = 0;
         for (let j = 0; j < array.length; j++) {
-            if (current > array[j]) {
+            if (current > arraySort[index]) {
                 index++;
             }
         }
