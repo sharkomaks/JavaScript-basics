a553707b430b8ecf4719932213d09e6aa97a5a05
Maksim <sharkomaks@gmail.com>
2024-02-26 19:59:57 +0300

    Домашнее задание

--- 10-array-clean/app.js
+++ 10-array-clean/app.js
@@ -1,16 +1,10 @@
 const arr = [1, 15, -3, 10, -17, 24, 38, -11];
 
 const filterArray = (array, removeFunc) => {
-    const filteredArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (!removeFunc(array[i])) {
-            filteredArray.push(array[i]);
-        }
-    }
-    return filteredArray;
+    return array.filter(number => !removeFunc(number));
 }
 
 const shouldRemove = num => num > 2;
 
 const filter = filterArray(arr, shouldRemove);
-console.log(filter)
+console.log(filter);
--- 15-task-object/app.js
+++ 15-task-object/app.js
@@ -1,11 +1,15 @@
+"use strict";
+
 const toDoList = {
-    tasks: [{
-        title: 'Помыть посуду',
-        id: 1,
-        priority: 1,
-    }],
+    tasks: [],
+    id: 0,
     addTask: function (task) {
-        this.tasks.push(task)
+        if (task) {
+            this.id++;
+            task.id = this.id;
+            this.tasks.push(task);
+            return this.id
+        }
     },
     deleteTaskById: function (idNumber) {
         this.tasks = this.tasks.filter(task => task.id !== idNumber);
@@ -24,18 +28,19 @@ const toDoList = {
 };
 
 toDoList.addTask({
+    title: 'Помыть посуду',
+    priority: 1,
+});
+toDoList.addTask({
     title: 'Погулять с собакой',
-    id: 2,
     priority: 1,
 });
 toDoList.addTask({
     title: 'Выбросить мусор',
-    id: 3,
     priority: 1,
 });
 toDoList.addTask({
     title: 'Пропылесосить',
-    id: 4,
     priority: 2,
 });
 console.log(toDoList.tasks);
@@ -45,3 +50,4 @@ toDoList.renameById(3, 'Купить продуктов', 3);
 console.log(toDoList.tasks);
 toDoList.taskSort();
 console.log(toDoList.tasks);
+console.log(toDoList.id)
